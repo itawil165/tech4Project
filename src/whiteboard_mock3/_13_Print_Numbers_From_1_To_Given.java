d9ae7d6603b7885b7f5a5e921b61ac6413a0c264
Ismail Tawil <itawil165@gmail.com>
2022-05-01 00:02:19 -0500

    Tech Global Java code committed

--- src/whiteboard_mock3/_13_Print_Numbers_From_1_To_Given.java
+++ src/whiteboard_mock3/_13_Print_Numbers_From_1_To_Given.java
@@ -1,4 +1,15 @@
 package whiteboard_mock3;
 
 public class _13_Print_Numbers_From_1_To_Given {
+
+    public static void nums (int a) {
+
+        for (int i = 0; i < a; i++) {
+            System.out.print(i + ", ");
+        }
+    }
+
+    public static void main(String[] args) {
+        nums(5);
+    }
 }
