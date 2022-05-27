c5aa67433e5258484a5bc048f1d5c19efb6d330e
Ismail Tawil <itawil165@gmail.com>
2022-05-27 03:49:03 -0500

    Tech Global Java code committed

--- src/homework/Homework19.java
+++ src/homework/Homework19.java
@@ -1,9 +1,7 @@
 package homework;
 
-import primitives.Int;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Homework19 {
@@ -20,7 +18,15 @@ public class Homework19 {
 
     // TASK 03
     public static int sumOfDigits (String str) {
-        return 0;
+        char[] c = str.toCharArray();
+        int sum = 0;
+        for (char value : c) {
+            if (Character.isDigit(value)) {
+                int a = Integer.parseInt(String.valueOf(value));
+                sum += a;
+            }
+        }
+        return sum;
     }
 
     // TASK 04
@@ -44,7 +50,48 @@ public class Homework19 {
         return intArr.get(1);
     }
 
-    public static void main(String[] args) {
+    // TASK 06
+    public static int[] no13 (int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 13) arr[i] = 0;
+        }
+        return arr;
+    }
+
+    // TASK 07
+    public static int[] arrFactorial (int[] factorial) {
+
+        int fact;
+        for (int i = 0; i < factorial.length; i++) {
+            fact = 1;
+            for (int j = 1; j <= factorial[i]; j++) {
+                fact *= j;
+            }
+            factorial[i] = fact;
+        }
+        return factorial;
+    }
+
+    // TASK 08
+    public static String[] categorizeCharacters (String str) {
+        StringBuilder letters = new StringBuilder(),
+                numbers = new StringBuilder(), special = new StringBuilder();
+
+        for (int i=  0; i < str.length(); i++)
+        {
+            if (Character.isLetter(str.charAt(i)))
+                letters.append(str.charAt(i));
+            else if(Character.isDigit(str.charAt(i)))
+                numbers.append(str.charAt(i));
+            else
+                special.append(str.charAt(i));
+        }
+        return new String[]{String.valueOf(letters), String.valueOf(numbers), String.valueOf(special)};
+    }
+
+
+
+        public static void main(String[] args) {
 
         // TASK 01
         System.out.println(noDigits("123Tech456Global789"));
@@ -53,11 +100,21 @@ public class Homework19 {
         System.out.println(noVowels("TechGlobal"));
 
         // TASK 03
+        System.out.println(sumOfDigits("John's age is 29"));
 
         // TASK 04
         System.out.println(hasUpperCase("TechGlobal"));
 
         // TASK 05
         System.out.println(middleInt(-1, 25, 10));
+
+        // TASK 06
+        System.out.println(Arrays.toString(no13(new int[]{13, 1, 13, 6})));
+
+        // TASK 07
+        System.out.println(Arrays.toString(arrFactorial(new int[]{5, 0, 6})));
+
+        // TASK 08
+        System.out.println(Arrays.toString(categorizeCharacters("abc123!@#")));
     }
 }
