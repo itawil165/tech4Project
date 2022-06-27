6cccdafe02407c573128118c3a50480d533a0bc4
Ismail Tawil <itawil165@gmail.com>
2022-06-27 01:29:40 -0500

    Tech Global Java code committed

--- src/homework/Homework21.java
+++ src/homework/Homework21.java
@@ -1,4 +1,88 @@
 package homework;
 
 public class Homework21 {
+
+    // TASK 01
+    public static void fizzBuzz1 (int num) {
+        for (int i = 1; i <= num; i++) {
+            if (i % 15 == 0) System.out.print("FizzBuzz, ");
+            else if (i % 3 == 0) System.out.print("Fizz, ");
+            else if (i % 5 == 0) System.out.print("Buzz, ");
+            else System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+
+    // TASK 02
+    public static String fizzBuzz2 (int num){
+        String str = "";
+        for (int i = 1; i <= num; i++) {
+            if (i % 15 == 0) str = "FizzBuzz";
+            else if (i % 3 == 0) str = "Fizz";
+            else if (i % 5 == 0) str = "Buzz";
+            else str = String.valueOf(num);
+        }
+        return str;
+    }
+
+    // TASK 03
+    public static int findSumNumbers (String str){
+        int sum = 0;
+        String temp = "0";
+        for (int i = 0; i < str.length(); i++){
+            if (Character.isDigit(str.charAt(i))) temp += str.charAt(i);
+            else {
+                sum += Integer.parseInt(temp);
+                temp = "0";
+            }
+        }return sum + Integer.parseInt(temp);
+    }
+
+    // TASK 04
+    public static int findBiggestNumber (String str){
+        int max = 0;
+        String temp = str.replaceAll("[^\\d]", " ");
+        String[] temp2 = temp.replaceAll("[\\s]+", " ").split(" ");
+        for (String element: temp2){
+            if (!element.isEmpty()){
+                if (Integer.parseInt(element) > max) max = Integer.parseInt(element);
+            }
+        }
+        return max;
+    }
+
+    // TASK 05
+    public static String countSequenceOfCharacters (String str){
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count ++;
+            }
+            else {
+                count ++;
+                result.append(count).append(str.charAt(i));
+                count = 0;
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        // TASK 01
+        fizzBuzz1(5);
+
+        // TASK 02
+        System.out.println(fizzBuzz2(5));
+
+        // TASK 03
+        System.out.println(findSumNumbers("a1b4c 6#"));
+
+        // TASK 04
+        System.out.println(findBiggestNumber("a1b4c 6#"));
+
+        // TASK 05
+        System.out.println(countSequenceOfCharacters("abbcca"));
+    }
 }
--- src/homework/Homework23.java
+++ src/homework/Homework23.java
@@ -1,4 +1,75 @@
 package homework;
 
+import primitives.Int;
+
+import java.util.*;
+
 public class Homework23 {
+
+    // TASK 01
+    public static LinkedList<String> parseData (Map<Integer, String> parseMap) {
+
+        LinkedList<String> list = new LinkedList<>();
+
+        for (Map.Entry<Integer, String> parse : parseMap.entrySet()) {
+            String s = parse.getKey() + " = " + parse.getValue();
+            list.add(parse.getKey() + " = " + parse.getValue());
+        }
+        return list;
+    }
+
+    // TASK 02
+    public static double calculateTotalPrice1 (double appleAmnt, double orangeAmnt, double mangoAmnt, double pineappleAmnt) {
+
+        Map<String, Double> priceMap = new TreeMap<>();
+        priceMap.put("Apple", 2.00);
+        priceMap.put("Orange", 3.29);
+        priceMap.put("Mango", 4.99);
+        priceMap.put("Pineapple", 5.25);
+
+        return (priceMap.get("Apple") * appleAmnt) + (priceMap.get("Orange") * orangeAmnt)
+                + (priceMap.get("Mango") * mangoAmnt) + (priceMap.get("Pineapple") * pineappleAmnt);
+    }
+
+    // TASK 03
+    public static double calculateTotalPrice2 (double appleAmnt, double orangeAmnt, double mangoAmnt) {
+
+        Map<String, Double> priceMap = new TreeMap<>();
+        priceMap.put("Apple", 2.00);
+        priceMap.put("Orange", 3.29);
+        priceMap.put("Mango", 4.99);
+
+        double appleDiscount = 1;
+        if (appleAmnt % 2 == 0) appleDiscount = 0.75;
+        else if (appleAmnt % 3 == 0) appleDiscount = 5.0 / 6.0;
+
+        double freeMangoAmnt = 0;
+        if (mangoAmnt % 4 == 0) freeMangoAmnt = mangoAmnt / 4;
+
+
+        double applePrice = (priceMap.get("Apple") * appleAmnt) * appleDiscount;
+        double orangePrice = (priceMap.get("Orange") * orangeAmnt);
+        double mangoPrice = (priceMap.get("Mango") * mangoAmnt) - (priceMap.get("Mango") * freeMangoAmnt);
+        return applePrice + orangePrice + mangoPrice;
+    }
+
+
+    public static void main(String[] args) {
+
+        // TASK 01
+        Map<Integer, String> parseMap = new TreeMap<>();
+        parseMap.put(104, "LA");
+        parseMap.put(103, "Paris");
+        parseMap.put(102, "Berlin");
+        parseMap.put(101, "Chicago");
+        parseMap.put(100, "London");
+
+        System.out.println(parseData(parseMap));
+
+        // TASK 02
+        System.out.println(calculateTotalPrice1(3,0,1,0));
+
+        // TASK 03
+        System.out.println(calculateTotalPrice2(3,0,5));
+    }
 }
